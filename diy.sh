05c5aa49a14d62ba7d5aa77a83a4c7918f9f30bc
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-02-25 23:02:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,15 +6,16 @@
 #   Blog: https://p3terx.com
 #=================================================
 #克隆源码
-git clone -b openwrt-19.07 https://github.com/openwrt/openwrt.git openwrt
+#git clone -b openwrt-19.07 https://github.com/openwrt/openwrt.git openwrt
 git clone https://github.com/coolsnowwolf/lede lede
-ln -s ../../lede/package/lean ./openwrt/package/
-cd openwrt
-sed -i '/luci/d' feeds.conf.default
-sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
-sed -i '$a src-git extra https://github.com/Andy2244/openwrt-extra.git' feeds.conf.default
+#ln -s ../../lede/package/lean ./openwrt/package/
+#cd openwrt
+cd lede
+#sed -i '/luci/d' feeds.conf.default
+#sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
+#sed -i '$a src-git extra https://github.com/Andy2244/openwrt-extra.git' feeds.conf.default
 ./scripts/feeds update -a
-./scripts/feeds install -f -p extra -a
+#./scripts/feeds install -f -p extra -a
 ./scripts/feeds install -a
 #添加自定义插件
 git clone https://github.com/Ameykyl/luci-app-koolproxyR.git package/luci-app-koolproxyR
@@ -37,7 +38,7 @@ ln -s ../../luci-theme-argon1.x ./package/
 ln -s ../../luci-app-flowoffload_ADGHome ./package/
 
 #修改lan口地址
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.5/g' package/base-files/files/bin/config_generate
 
 #修改机器名称
 sed -i 's/OpenWrt/Newifi-Y1/g' package/base-files/files/bin/config_generate
@@ -49,11 +50,10 @@ sed -i 's/OpenWrt/FK20100010/g' package/kernel/mac80211/files/lib/wifi/mac80211.
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 #修改zzz-default-settings的配置
-sed -i '46,48d' package/lean/default-settings/files/zzz-default-settings
+#sed -i '46,48d' package/lean/default-settings/files/zzz-default-settings
 sed -i '/exit 0/i\chmod 755 /etc/init.d/serverchan' package/lean/default-settings/files/zzz-default-settings
 sed -i '/exit 0/i\chmod 755 /usr/bin/serverchan/serverchan' package/lean/default-settings/files/zzz-default-settings
 sed -i '/exit 0/i\echo 0xDEADBEEF > /etc/config/google_fu_mode\n' package/lean/default-settings/files/zzz-default-settings
-sed -i '/exit 0/i\rm -rf /usr/bin/v2ray' package/lean/default-settings/files/zzz-default-settings
 
 #修改banner
 rm -rf package/base-files/files/etc/banner
