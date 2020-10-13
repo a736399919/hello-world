a932a602250ae4838876e26891213dc9adb8bdf4
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-10-14 04:08:35 +0800

    Update y1.sh

--- y1.sh
+++ y1.sh
@@ -9,9 +9,12 @@ git clone https://github.com/coolsnowwolf/lede openwrt
 cd openwrt
 #添加Lienol的插件包
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git oui https://github.com/zhaojh329/oui.git' feeds.conf.default
 ./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+./scripts/feeds update oui
+./scripts/feeds install -a -p oui
 #添加自定义插件
 #git clone https://github.com/Ameykyl/luci-app-koolproxyR.git package/luci-app-koolproxyR
 #git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
@@ -32,12 +35,12 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.
 ln -s ../../luci-theme-argon1.x ./package/
 ln -s ../../luci-app-flowoffload_ADGHome ./package/
 #添加主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.5.1
-rm -rf package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/head-icon.jpg
-rm -rf package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/img/
-cp -rf ../luci-theme-argon1.x/htdocs/luci-static/argon/head-icon.jpg package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/
-sed -i '/class="darkMask"/a \ \ \ <div class="login-bg" style="background-color: #5e72e4"></div>' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
-sed -i '/background-image/d' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.0
+#rm -rf package/luci-theme-argon-1.7.0/htdocs/luci-static/argon/head-icon.jpg
+#rm -rf package/luci-theme-argon-1.7.0/htdocs/luci-static/argon/img/
+#cp -rf ../luci-theme-argon1.x/htdocs/luci-static/argon/head-icon.jpg package/luci-theme-argon-1.7.0/htdocs/luci-static/argon/
+#sed -i '/class="darkMask"/a \ \ \ <div class="login-bg" style="background-color: #5e72e4"></div>' package/luci-theme-argon-1.7.0/luasrc/view/themes/argon/header.htm
+#sed -i '/background-image/d' package/luci-theme-argon-1.7.0/luasrc/view/themes/argon/header.htm
 
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.1
 rm -rf package/luci-theme-argon-2.1/htdocs/luci-static/argon/head-icon.jpg
