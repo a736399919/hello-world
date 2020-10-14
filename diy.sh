f80589ccaecd8e6c9d88963f887712568c57dd6b
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-10-15 00:47:44 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,23 +8,20 @@
 #克隆源码
 git clone https://github.com/openwrt/openwrt.git openwrt
 cd openwrt
-#sed -i '/luci/d' feeds.conf.default 删除含luci关键词的行
-sed -i '$a src-git oui https://github.com/zhaojh329/oui.git' feeds.conf.default
 ./scripts/feeds update -a
-./scripts/feeds update oui
-./scripts/feeds install -a -p oui
+./scripts/feeds install -a
 
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #修改机器名称
-sed -i 's/OpenWrt/Newifi-Y1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/MiWiFi/g' package/base-files/files/bin/config_generate
 
 #默认打开WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #修改wifi名称
-sed -i 's/OpenWrt/FK20100010/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/MiWiFi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #修改时区
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
@@ -37,5 +34,5 @@ sed -i 's/OpenWrt/FK20100010/g' package/kernel/mac80211/files/lib/wifi/mac80211.
 
 #修改banner
 rm -rf package/base-files/files/etc/banner
-cp -f ../banner-y1 package/base-files/files/etc/banner
-cp -f ../oui.config .config
+cp -f ../banner-miwifi package/base-files/files/etc/banner
+cp -f ../openwrt-miwifi.config .config
