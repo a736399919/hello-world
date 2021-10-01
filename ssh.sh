7e449def97dcf7d0ac22760fa09b7a6da30822ae
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-10-02 07:58:21 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -10,3 +10,9 @@ wget --no-check-certificate http://downloads.pangubox.com:6380/pandorabox/19.01/
 tar xvf PandoraBox-SDK-ralink-mt7620_gcc-5.5.0_uClibc-1.0.x.Linux-x86_64-2018-12-31-git-4b6a3d5ca.tar.xz
 mv PandoraBox-SDK-ralink-mt7620_gcc-5.5.0_uClibc-1.0.x.Linux-x86_64-2018-12-31-git-4b6a3d5ca openwrt
 git clone https://github.com/Boos4721/openwrt openwrt1
+mv openwrt1/feeds.conf.default openwrt
+mv openwrt1/package/lean openwrt/package/
+cd openwrt
+
+./scripts/feeds update -a
+./scripts/feeds install -a
