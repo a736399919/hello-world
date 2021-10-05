1666232a51b74d5771890bed06472dc82e366890
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-10-05 10:20:25 +0800

    Update mi.sh

--- mi.sh
+++ mi.sh
@@ -8,7 +8,7 @@
 git clone https://github.com/coolsnowwolf/lede openwrt
 cd openwrt
 #添加Lienol的插件包
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 ./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
@@ -28,31 +28,22 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.
 #rm -rf package/lean/qBittorrent/patches
 #cp -f ../qb421 package/lean/qBittorrent/Makefile
 
-#添加自己repo的插件的软连接
-ln -s ../../luci-theme-argon1.x ./package/
-ln -s ../../luci-app-flowoffload_ADGHome ./package/
 #添加主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.0
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
 #rm -rf package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/head-icon.jpg
 #rm -rf package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/img/
 #cp -rf ../luci-theme-argon1.x/htdocs/luci-static/argon/head-icon.jpg package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/
 #sed -i '/class="darkMask"/a \ \ \ <div class="login-bg" style="background-color: #5e72e4"></div>' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
 #sed -i '/background-image/d' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
 
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.1
-rm -rf package/luci-theme-argon-2.1/htdocs/luci-static/argon/head-icon.jpg
-rm -rf package/luci-theme-argon-2.1/htdocs/luci-static/argon/img/
-cp -rf ../luci-theme-argon1.x/htdocs/luci-static/argon/head-icon.jpg package/luci-theme-argon-2.1/htdocs/luci-static/argon/
-sed -i '/class="darkMask"/a \ \ \ <div class="login-bg" style="background-color: #5e72e4"></div>' package/luci-theme-argon-2.1/luasrc/view/themes/argon/header.htm
-sed -i '/background-image/d' package/luci-theme-argon-2.1/luasrc/view/themes/argon/header.htm
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #修改机器名称
-sed -i 's/OpenWrt/MIWIFI/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Y1IWIFI/g' package/base-files/files/bin/config_generate
 
 #修改wifi名称
-sed -i 's/OpenWrt/MIWIFI/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/Y1WIFI/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #默认打开WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
@@ -69,7 +60,7 @@ sed -i '/exit 0/i\chmod 775 /usr/bin/webd' package/lean/default-settings/files/z
 
 #修改banner
 rm -rf package/base-files/files/etc/banner
-cp -f ../banner-miwifi package/base-files/files/etc/banner
+cp -f ../banner-y1 package/base-files/files/etc/banner
 #[ -e ../files ] && mv ../files files
 [ -e ../mi-default.config ] && mv ../mi-default.config .config
 cp -rf ../lean-mi.config .config
