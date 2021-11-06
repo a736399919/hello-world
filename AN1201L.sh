c6f2ad16ac73aed301ac13ca9b215558c4c82141
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-11-07 01:39:11 +0800

    Update AN1201L.sh

--- AN1201L.sh
+++ AN1201L.sh
@@ -14,6 +14,14 @@ sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+sed -i "s/hiwifi_hc5962/cmcc_an1201l/g" `grep hiwifi_hc5962 -rl target`
+sed -i "s/HC5962/AN1201L/g" `grep HC5962 -rl target`
+sed -i "s/hc5962/an1201l/g" `grep hc5962 -rl target`
+sed -i "s/HiWiFi/CMCC/g" `grep HiWiFi -rl target`
+sed -i "s/hiwifi/CMCC/g" `grep hiwifi -rl target`
+cp target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts target/linux/ramips/dts/mt7621_cmcc_an1201l.dts
+touch target/linux/*/Makefile
+
 #添加主题
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge package/luci-theme-edge
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
@@ -30,5 +38,5 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 #加载config
-[ -e ../config/an1201l.config ] && mv -f ../config/an1201l.config .config
+[ -e ../config/an1201l-1.config ] && mv -f ../config/an1201l-1.config .config
 
