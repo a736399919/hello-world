17606f4bc6dc09a17427b14e7cbc2fd55f0da620
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-12-12 13:40:08 +0800

    Update AN1201L-Lienol.sh

--- AN1201L-Lienol.sh
+++ AN1201L-Lienol.sh
@@ -6,7 +6,7 @@
 #   Blog: https://p3terx.com
 #=================================================
 #克隆源码
-git clone -b main --single-branch https://github.com/Lienol/openwrt openwrt
+git clone -b 21.02 --single-branch https://github.com/Lienol/openwrt openwrt
 cd openwrt
 #添加passwall
 sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
