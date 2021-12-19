7e58f71427fd406d3b2024ebcd0130669f99c04b
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-12-19 15:33:28 +0800

    Update and rename y1.sh to Y1_Lienol.sh

---
+++ Y1_Lienol.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#=================================================
+#   Description: DIY script
+#   Lisence: MIT
+#   Author: P3TERX
+#   Blog: https://p3terx.com
+#=================================================
+#克隆源码
+git clone -b main --single-branch https://github.com/Lienol/openwrt openwrt
+cd openwrt
+#添加passwall
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+#添加自定义插件
+git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
+#添加主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.2.9
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge package/luci-theme-edge
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
+
+#修改lan口地址
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate                                  
+#修改机器名称
+sed -i 's/OpenWrt/Newifi_Mini/g' package/base-files/files/bin/config_generate
+#修改wifi名称
+sed -i 's/OpenWrt/Newifi_Mini/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#默认打开WiFi
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#修改时区
+#sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner-miwifi package/base-files/files/etc/banner
+#加载config
+[ -e ../config/Y1_Lienol.config ] && mv -f ../config/Y1_Lienol.config .config
