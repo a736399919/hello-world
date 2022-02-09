248b0462510c6982606f078740c68c088c994186
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-02-10 00:27:44 +0800

    Update AN1201L-Lienol.sh

--- AN1201L-Lienol.sh
+++ AN1201L-Lienol.sh
@@ -15,36 +15,24 @@ sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-chmod 755 ../config/lienol_02_network
-mv ../config/lienol_mt7621_xiaomi_router-ac2100.dtsi target/linux/ramips/dts/mt7621_xiaomi_router-ac2100.dtsi
-mv ../config/lienol_mt7621_xiaomi_mi-router-3g.dts target/linux/ramips/dts/mt7621_xiaomi_mi-router-3g.dts
-mv ../config/lienol_02_network target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-#sed -i "s/hiwifi_hc5962/cmcc_an1201l/g" `grep hiwifi_hc5962 -rl target`
-#sed -i "s/HC5962/AN1201L/g" `grep HC5962 -rl target`
-#sed -i "s/hc5962/an1201l/g" `grep hc5962 -rl target`
-#sed -i "s/HiWiFi/CMCC/g" `grep HiWiFi -rl target`
-#sed -i "s/hiwifi/cmcc/g" `grep hiwifi -rl target`
-#cp target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts target/linux/ramips/dts/mt7621_cmcc_an1201l.dts
-#touch target/linux/*/Makefile
-
 #添加自定义插件
-#git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
+git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
 git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
 #添加主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.2.9
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge package/luci-theme-edge
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
+#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge package/luci-theme-edge
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
 
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate                                  
 #修改机器名称
-sed -i 's/OpenWrt/AN1201L/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/R4A/g' package/base-files/files/bin/config_generate
 #修改wifi名称
-sed -i 's/OpenWrt/AN1201L/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/R4A/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #默认打开WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #修改时区
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 #加载config
-[ -e ../config/Lienol_miwifi-3G.config ] && mv -f ../config/Lienol_miwifi-3G.config .config
+#[ -e ../config/Lienol_R4A.config ] && mv -f ../config/Lienol_R4A.config .config
