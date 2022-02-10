09210f59496b67592f97e9dbbfd3e69f8d638496
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-02-10 19:34:59 +0800

    Update AN1201L-Lienol.sh

--- AN1201L-Lienol.sh
+++ AN1201L-Lienol.sh
@@ -15,11 +15,29 @@ sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-chmod 755 ../config/pbr_m1_02_network
-#mv ../config/pbr_m1_02_network target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-#mv ../config/mt7621_d-team_pbr-m1.dts target/linux/ramips/dts/mt7621_d-team_pbr-m1.dts
-mv ../config/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
-mv ../config/mt7621.mk target/linux/ramips/image/mt7621.mk
+### 修改为R4A千兆版Breed直刷版
+## mt7621_xiaomi_mir3g-v2.dts 好像被改成了 mt7621_xiaomi_mi-router-4a-3g-v2.dtsi  测试一下
+## 1.修改 mt7621_xiaomi_mir3g-v2.dts
+export shanchu1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
+export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
+export shanchu2=$(expr $shanchu2 - 1)
+export shanchu2=$(echo $shanchu2"d")
+sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
+grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
+echo "" >> youhua.txt
+echo "" >> youhua.txt
+export shanchu1=$(expr $shanchu1 - 1)
+export shanchu1=$(echo $shanchu1"r")
+sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
+rm -rf youhua.txt
+## 2.修改mt7621.mk
+export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mir3g-v2' target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
+export imsize1=$(expr $imsize1 + 2)
+export imsize1=$(echo $imsize1"s")
+sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/image/mt7621.mk
+
+#更改主机型号，支持中文。 
+sed -i "s/Xiaomi Mi Router 4A Gigabit Edition/辣鸡小米4A千兆版/g" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit.dts
 
 #添加自定义插件
 git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
