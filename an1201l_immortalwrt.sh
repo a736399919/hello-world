babbe06dce4dc12ab75d03027d9251b401791132
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-02-10 18:55:26 +0800

    Update an1201l_immortalwrt.sh

--- an1201l_immortalwrt.sh
+++ an1201l_immortalwrt.sh
@@ -11,19 +11,27 @@ cd openwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-mv ../config/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
-mv ../config/mt7621.mk target/linux/ramips/image/mt7621.mk
+### 修改为R4A千兆版Breed直刷版
+## mt7621_xiaomi_mir3g-v2.dts 好像被改成了 mt7621_xiaomi_mi-router-4a-3g-v2.dtsi  测试一下
+## 1.修改 mt7621_xiaomi_mir3g-v2.dts
+export shanchu1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
+export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
+export shanchu2=$(expr $shanchu2 - 1)
+export shanchu2=$(echo $shanchu2"d")
+sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
+grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
+echo "" >> youhua.txt
+echo "" >> youhua.txt
+export shanchu1=$(expr $shanchu1 - 1)
+export shanchu1=$(echo $shanchu1"r")
+sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
+rm -rf youhua.txt
+## 2.修改mt7621.mk
+export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mir3g-v2' target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
+export imsize1=$(expr $imsize1 + 2)
+export imsize1=$(echo $imsize1"s")
+sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/image/mt7621.mk
 
-#chmod 755 ../config/immortalwrt_02_network
-#mv ../config/immortalwrt_mt7621_hiwifi_hc5962.dts target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
-#mv ../config/immortalwrt_02_network target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-#sed -i "s/hiwifi_hc5962/cmcc_an1201l/g" `grep hiwifi_hc5962 -rl target`
-#sed -i "s/HC5962/AN1201L/g" `grep HC5962 -rl target`
-#sed -i "s/hc5962/an1201l/g" `grep hc5962 -rl target`
-#sed -i "s/HiWiFi/CMCC/g" `grep HiWiFi -rl target`
-#sed -i "s/hiwifi/cmcc/g" `grep hiwifi -rl target`
-#cp target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts target/linux/ramips/dts/mt7621_cmcc_an1201l.dts
-#touch target/linux/*/Makefile
 
 #添加自定义插件
 git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
