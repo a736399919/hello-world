da4104ca4cca76d5c498cf7fd041fa6202701f83
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-04-11 06:38:40 +0800

    Update an1201l_immortalwrt.sh

--- an1201l_immortalwrt.sh
+++ an1201l_immortalwrt.sh
@@ -6,54 +6,37 @@
 #   Blog: https://p3terx.com
 #=================================================
 #克隆源码
-git clone -b openwrt-21.02 --single-branch https://github.com/immortalwrt/immortalwrt openwrt
+git clone -b openwrt-18.06-k5.4 --single-branch https://github.com/immortalwrt/immortalwrt openwrt
 cd openwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-### 修改为R4A千兆版Breed直刷版
-## mt7621_xiaomi_mir3g-v2.dts 好像被改成了 mt7621_xiaomi_mi-router-4a-3g-v2.dtsi  测试一下
-## 1.修改 mt7621_xiaomi_mir3g-v2.dts
-export shanchu1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
-export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
-export shanchu2=$(expr $shanchu2 - 1)
-export shanchu2=$(echo $shanchu2"d")
-sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
-grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
-echo "" >> youhua.txt
-echo "" >> youhua.txt
-export shanchu1=$(expr $shanchu1 - 1)
-export shanchu1=$(echo $shanchu1"r")
-sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
-rm -rf youhua.txt
-## 2.修改mt7621.mk
-export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mi-router-4a-gigabit' target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
-export imsize1=$(expr $imsize1 + 3)
-export imsize1=$(echo $imsize1"s")
-sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 32448k/" target/linux/ramips/image/mt7621.mk
-
+mv ../config/immortalwrt_mt7621_youhua_wr1200js.dts target/linux/ramips/dts/mt7621_youhua_wr1200js.dts
+mv ../config/immortalwrt_02_network target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 #更改主机型号，支持中文。 
-sed -i "s/Xiaomi Mi Router 4A Gigabit Edition/辣鸡小米4A千兆版/g" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit.dts
+sed -i "s/YouHua WR1200JS/CMCC_AN1201L/g" target/linux/ramips/dts/mt7621_youhua_wr1200js.dts
 
 #添加自定义插件
 git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
 git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
+
 #添加主题
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.2.9
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge package/luci-theme-edge
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.2.9
 
 #修改lan口地址
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate                                  
+#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate                                  
 #修改机器名称
-sed -i 's/OpenWrt/R4A/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/AN1201L/g' package/base-files/files/bin/config_generate
 #修改wifi名称
-sed -i 's/OpenWrt/R4A/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/AN1201L/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #默认打开WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #修改时区
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 #加载config
-mv -f ../config/r4a-pw.config.new .config
+mv -f ../config/immortalwrt_an1201l.config .config
